99bb777d142af2a2489e8566c416a21e3d9889e3
reitor <reitor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-22 18:50:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-mypost/tags/1.2@279345 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tweet-mypost.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: Tweet myPost
+Plugin URI: http://reitor.org/wp-plugins/tweet-mypost/
+Description: Send to twitter the posts published, using your Twitter App OAuth.
+Version: 1.2
+Author: Reitor
+Author URI: http://reitor.org/
+*/
+
+load_plugin_textdomain(
+	'tweet_mypost', 
+	false, 
+	'tweet-mypost/lang'
+);
+function tweetmp_config(){
+if(!current_user_can('manage_options')) {
+	wp_die(__('Unallowed', 'tweet_mypost'));
+}
+	include('tweetmp_config.php');
+}
+function tmp_tweet($msg){
+	require_once 'EpiCurl.php';
+	require_once 'EpiOAuth.php';
+	require_once 'EpiTwitter.php';
+	$twitterObj	= new EpiTwitter(get_option('tweetmp_consumerkey'),get_option('tweetmp_consumersecret'),get_option('tweetmp_token'),get_option('tweetmp_secret'));
+	$tweet		= $twitterObj->post_statusesUpdate(array('status' => $msg));
+	$tweet->responseText;
+}
+function tmp_shorturl($url){
+	$api_url	= str_replace('%link%',$url,get_option('tweetmp_shorturl'));
+	$ch		= curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $api_url);
+	$s_url		= curl_exec($ch);
+	return trim($s_url);
+}
+function tweetmypost(){
+	global $post;
+	$post_url		= tmp_shorturl(trim(get_permalink($post->ID)));
+	$title			= $post->post_title;
+	$search			= array('%title%', '%link%');
+	$now			= array($title, $post_url);
+	$tweet_mypost	= str_replace($search,$now,get_option('tweetmp_formatpost'));
+	if($_POST['tweetm_post'] == 'yes'){
+		tmp_tweet($tweet_mypost);
+	}
+}
+function tweetmp_menu(){
+	if(function_exists('add_menu_page')){
+		add_menu_page(__('Tweet myPost &lsaquo; Settings', 'tweet_mypost'), 'Tweet myPost', 'manage_options', 'tweet_mypost', 'tweetmp_config');
+	}
+}
+function Tweetmp_init(){
+	wp_register_style('TweetmpStyle', WP_PLUGIN_URL . '/tweet-mypost/css/style.css');
+}
+function addTweetmpStyle(){
+	wp_enqueue_style('TweetmpStyle');
+}
+if(get_option('tweetmp_token') == ""){
+add_action('admin_notices', 'tweetmp_warning');
+}
+function tweetmp_warning(){
+	echo '<div class="error"><p><a href="'.get_bloginfo('wpurl').'/wp-admin/admin.php?page=tweet_mypost">'.__('Please Configure <strong>Tweet myPost</strong>', 'tweet_mypost').'</a></p></div>';
+}
+function tweetmp_install(){
+	add_option('tweetmp_token');
+	add_option('tweetmp_secret');
+	add_option('tweetmp_consumerkey');
+	add_option('tweetmp_consumersecret');
+	add_option('tweetmp_typeshort','migre.me');
+	add_option('tweetmp_shorturl','http://migre.me/api.txt?url=%link%');
+	add_option('tweetmp_formatpost','%title% - %link%');
+}
+function tweetmp_uninstall(){
+	delete_option('tweetmp_token');
+	delete_option('tweetmp_secret');
+	delete_option('tweetmp_consumerkey');
+	delete_option('tweetmp_consumersecret');
+	delete_option('tweetmp_shorturl');
+	delete_option('tweetmp_typeshort');
+	delete_option('tweetmp_formatpost');
+}
+add_action('admin_menu','tmpost_box');
+function tmpost_box(){
+	if( function_exists('add_meta_box')){
+		add_meta_box('tweetmp','Tweet myPost','tweetmp_box','post','side','high');
+	}
+}
+function tweetmp_box(){
+	global $post;
+	if(($post->post_status) != 'publish'){
+?>
+<label><input id="tweetm_post" type="checkbox" name="tweetm_post" value="yes" checked /> <?php echo __('Send to Twitter', 'tweet_mypost');?></label>
+<?php
+	}else{?>
+<label><input id="tweetm_post" type="checkbox" name="tweetm_post" value="yes" disabled /> <?php echo __('Send to Twitter', 'tweet_mypost');?></label>
+<?php
+	}
+}
+add_action('admin_init', 'Tweetmp_init');
+add_action('admin_menu', 'tweetmp_menu');
+add_action('publish_post', 'tweetmypost');
+add_action('admin_print_styles', 'addTweetmpStyle');
+register_activation_hook(__FILE__, 'tweetmp_install');
+register_deactivation_hook(__FILE__, 'tweetmp_uninstall' );
+?>
---
+++ tweetmp_config.php
@@ -0,0 +1,126 @@
+<div class="wrap">
+<div id="icon-options-general" class="icon32"><br /></div>
+<h2><?php echo __('Tweet myPost Settings', 'tweet_mypost');?></h2>
+<?php
+if (isset($_POST['submit'])) {
+	update_option('tweetmp_consumerkey', trim($_POST['set_ckey']));
+	update_option('tweetmp_consumersecret', trim($_POST['set_csec']));
+	update_option('tweetmp_token', trim($_POST['set_token']));
+	update_option('tweetmp_secret', trim($_POST['set_secret']));
+	update_option('tweetmp_typeshort', trim($_POST['type_short']));
+	update_option('tweetmp_formatpost', $_POST['set_formatpost']);
+	if($_POST['type_short'] == 'migre.me'){
+		update_option('tweetmp_shorturl', 'http://migre.me/api.txt?url=%link%');
+	}
+	if($_POST['type_short'] == 'vai.la'){
+		update_option('tweetmp_shorturl', 'http://vai.la/link/api/?url=%link%');
+	}
+	if($_POST['type_short'] == 'is.gd'){
+		update_option('tweetmp_shorturl', 'http://is.gd/api.php?longurl=%link%');
+	}
+	if($_POST['type_short'] == 'u.nu'){
+		update_option('tweetmp_shorturl', 'http://u.nu/unu-api-simple?url=%link%');
+	}
+	if($_POST['type_short'] == 'custom'){
+		update_option('tweetmp_shorturl', trim($_POST['custom_short']));
+	}
+?>
+    <div class="updated">
+    	<p><strong><?php echo __('Settings updated.', 'tweet_mypost');?></strong></p>
+    </div>
+    <?php
+}
+?>
+<form method="post">
+<table border="0" cellpadding="5" cellspacing="10" width="100%">
+<tr>
+<td valign="top" width="50%">
+<div id="poststuff">
+<div class="postbox">
+<h3 class="hndle"><?php echo __('OAuth Twitter Settings', 'tweet_mypost');?></h3>
+<div class="inside">
+<p>Consumer Key: <br>
+<input type="text" name="set_ckey" size="60" value="<?php echo get_option('tweetmp_consumerkey'); ?>" /></p>
+<p>Consumer Secret: <br>
+<input type="text" name="set_csec" size="60" value="<?php echo get_option('tweetmp_consumersecret'); ?>" /></p>
+<p>Access Token: <br>
+<input type="text" name="set_token" size="60" value="<?php echo get_option('tweetmp_token'); ?>" /></p>
+<p>Access Token Secret: <br>
+<input type="text" name="set_secret" size="60" value="<?php echo get_option('tweetmp_secret'); ?>" /></p>
+</div>
+</div>
+</div>
+<div id="poststuff">
+<div class="postbox">
+<h3 class="hndle"><?php echo __('Format tweet', 'tweet_mypost');?></h3>
+<div class="inside">
+<p>
+<input type="text" name="set_formatpost" size="60" value="<?php echo get_option('tweetmp_formatpost'); ?>"/><br/>
+<small>%title% - <?php echo __('Post title', 'tweet_mypost');?><br/>
+%link% - <?php echo __('Post URL', 'tweet_mypost');?>
+</p>
+</div>
+</div>
+</div>
+<div id="poststuff">
+<div class="postbox">
+<h3 class="hndle"><?php echo __('Shorten', 'tweet_mypost');?></h3>
+<div class="inside">
+<table class="form-table">
+	<tr>
+		<td width="35%"><label><input name="type_short" type="radio" value="is.gd" <?php if(get_option('tweetmp_typeshort') == 'is.gd'){echo'checked';}?>/> is.gd</label></td>
+		<td width="65%"><label><input name="type_short" type="radio" value="migre.me" <?php if(get_option('tweetmp_typeshort') == 'migre.me'){echo'checked';}?>/> migre.me</label></td>
+	</tr>
+	<tr>
+		<td><label><input name="type_short" type="radio" value="vai.la" <?php if(get_option('tweetmp_typeshort') == 'vai.la'){echo'checked';}?>/> vai.la</label></td>
+		<td><label><input name="type_short" type="radio" value="u.nu" <?php if(get_option('tweetmp_typeshort') == 'u.nu'){echo'checked';}?>/> u.nu</label></td>
+	</tr>
+	<tr>
+		<td><label><input name="type_short" type="radio" value="custom" <?php if(get_option('tweetmp_typeshort') == 'custom'){echo'checked';}?>/> <?php echo __('Custom', 'tweet_mypost');?></label></td>
+		<td><input name="custom_short" size="35" type="text" value="<?php echo get_option('tweetmp_shorturl');?>" /></td>
+	</tr>
+</table>
+</div>
+</div>
+</div>
+</td>
+<div align="right">
+<input type="submit" name="submit" class="button-primary" value="<?php echo __('Update settings', 'tweet_mypost');?>" />
+</form>
+</div>
+<td valign="top" width="50%">
+<div id="poststuff">
+<div class="postbox">
+<h3 class="hndle"><?php echo __('Support', 'tweet_mypost');?></h3>
+<div class="inside">
+<p><?php echo __('Already have access token?', 'tweet_mypost');?><br/>
+<?php echo __('Register your <a href="http://dev.twitter.com/apps/new" target="_blank">application on Twitter</a>.', 'tweet_mypost');?></p>
+<p>
+<small>*<em><?php echo __('Step by step: <a href="http://www.reitor.org/wp-plugins/tweet-mypost/" target="_blank">www.reitor.org/wp-plugins/tweet-mypost/</a>', 'tweet_mypost');?></em></small>
+</p>
+</div>
+</div>
+</div>
+<div id="poststuff">
+<div class="postbox">
+<h3 class="hndle"><?php echo __('How to support me?', 'tweet_mypost');?></h3>
+<div class="inside">
+<p><strong><?php echo __('Send me bug reports, bugfixes, code modifications or your ideas.', 'tweet_mypost');?></strong><br/>
+<?php echo __('Help me to improve the plugin.', 'tweet_mypost');?></p>
+<p><strong><?php echo __('Make a translation of the plugin.', 'tweet_mypost');?></strong><br/>
+<?php echo __('The give other users more comfort help me to translate it to all languages.', 'tweet_mypost');?></p>
+<p><strong><?php echo __('Make A Donation', 'tweet_mypost');?></strong><br/>
+<form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="FPNFQ6GEZN43W">
+<input type="image" src="https://www.paypal.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - A maneira mais fácil e segura de efetuar pagamentos on-line!">
+<img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1">
+</form>
+</p>
+</div>
+</div>
+</div>
+</td>
+</tr>
+</table>
+</div>
