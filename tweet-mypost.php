eb7e23f19774315414f5ec4aab11fc69a9a3b9ee
reitor <reitor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-22 07:42:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-mypost/tags/1.1@279252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tweet-mypost.php
+++ tweet-mypost.php
@@ -89,14 +89,7 @@ function tweetmp_box(){
 <label><input id="tweetm_post" type="checkbox" name="tweetm_post" value="yes" checked /> Enviar para o Twitter</label>
 <?php
 	}else{?>
-<p>
-<form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="FPNFQ6GEZN43W">
-<input type="image" src="https://www.paypal.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - A maneira mais fácil e segura de efetuar pagamentos on-line!">
-<img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1">
-</form>
-</p>
+<label><input id="tweetm_post" type="checkbox" name="tweetm_post" value="yes" disabled /> Enviar para o Twitter</label>
 <?php
 	}
 }
